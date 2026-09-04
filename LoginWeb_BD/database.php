47077cd7439f2c834a9eee64062e30bc02e2fffb
JPdias15 <131601129+JPdias15@users.noreply.github.com>
2026-09-03 21:24:33 -0300

    Versão 2 - Banco de Dados

---
+++ LoginWeb_BD/database.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+    </head>
+    <body>
+        <?php
+           
+            $database = 'pweb';
+            $user = 'root';
+            $password = 'root';
+            $host = 'localhost';
+            
+            $conexao = mysqli_connect($host, $user, $password, $database);
+            
+        ?>
+    </body>
+</html>
